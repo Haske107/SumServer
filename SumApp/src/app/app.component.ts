e0fbf7bb168366bda2896e7d42bf47e7766213c7
Jeff <Haske107@mail.chapman.edu>
2020-04-03 14:59:22 -0700

    s

--- SumApp/src/app/app.component.ts
+++ SumApp/src/app/app.component.ts
@@ -1,9 +1,10 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
 import {transition, style, animate, trigger, state} from "@angular/animations";
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {MatSnackBar} from '@angular/material/snack-bar';
 import {MustMatch} from './helper/validate-password';
 import {HttpClient} from '@angular/common/http';
+import {isArray} from 'util';
 
 @Component({
   selector: 'app-root',
@@ -43,14 +44,16 @@ import {HttpClient} from '@angular/common/http';
 export class AppComponent implements OnInit, OnDestroy {
 
   // VARIABLES
-  Still_Photo_Selector = false;
-  Still_Photo_Counter = 1;
-  Still_Photo_ID = 1;
-  Still_Photo_TimeOut;
+  stillPhotoSelector = false;
+  stillPhotoCounter = 1;
+  stillPhotoID = 1;
+  stillPhotoTimeOut;
   registerForm: FormGroup;
   validated = true;
+  renderarray: any = [];
+  s3ContentLoaded = false;
 
-  constructor(private formBuilder: FormBuilder, private snackBar: MatSnackBar, private http: HttpClient) {
+  constructor(private formBuilder: FormBuilder, private snackBar: MatSnackBar, private http: HttpClient, private _ChangeDetectorRef: ChangeDetectorRef) {
     this.registerForm = this.formBuilder.group({
       password: ['', [Validators.required]],
     }, {
@@ -67,34 +70,41 @@ export class AppComponent implements OnInit, OnDestroy {
     this.photo_rotate_end();
   }
 
-
   // FUNCTION
     getRenderArray()  {
-      this.http.get("http://localhost:3000/getRenders")
-        .subscribe((data) =>  {
-         console.log(data);
+      this.http.get('http://localhost:3000/getRenders')
+        .subscribe((data: Array<any>) =>  {
+        data.forEach((render) => {
+           this.renderarray.push({
+             Key: render.Key,
+             Sentiment: parseInt(render.Sentiment, 10),
+             Chronology: parseInt(render.Chronology, 10)
+           });
         });
+        this._ChangeDetectorRef.detectChanges();
+      });
     }
     photo_rotate_start()  {
-      this.Still_Photo_TimeOut = setInterval(() =>  {
+      this.stillPhotoTimeOut = setInterval(() =>  {
         this.rotate_photos();
       }, 2000);
     }
     photo_rotate_end()  {
-      clearInterval(this.Still_Photo_TimeOut);
+      clearInterval(this.stillPhotoTimeOut);
     }
     rotate_photos() {
       // check if counter is in range
-      if (this.Still_Photo_Counter > 6)
-        this.Still_Photo_Counter = 1;
+      if (this.stillPhotoCounter > 6) {
+        this.stillPhotoCounter = 1;
+      }
       // flip selector
-      this.Still_Photo_Selector = !this.Still_Photo_Selector;
+      this.stillPhotoSelector = !this.stillPhotoSelector;
       // assign ID
-      if (this.Still_Photo_Selector)  {
-        this.Still_Photo_ID = this.Still_Photo_Counter;
-        this.Still_Photo_Counter += 1;
+      if (this.stillPhotoSelector)  {
+        this.stillPhotoID = this.stillPhotoCounter;
+        this.stillPhotoCounter += 1;
       } else  {
-        this.Still_Photo_ID = 0;
+        this.stillPhotoID = 0;
       }
 
 
@@ -107,7 +117,7 @@ export class AppComponent implements OnInit, OnDestroy {
     if (this.registerForm.invalid) {
       return;
     } else {
-      this.snackBar.open('Welcome, Friend', "", {
+      this.snackBar.open('Welcome, Friend', '', {
         duration: 3000
       });
       this.validated = true;
